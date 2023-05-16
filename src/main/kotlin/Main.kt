f430c52f8fafd1caf4c324bb75f974a7a564600d
Vasilii Karmanov <oldheffalump1@gmail.com>
2023-05-16 18:54:19 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    val amount: Int = 25000
+    val commission: Double = 0.75
+    val minCommission: Int = 35
+
+    val commissionsResult =  if (amount * commission / 100 > minCommission) amount * commission / 100 else (minCommission)
+
+    println("Commission is : $commissionsResult")
+
+}
